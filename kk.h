33ede6594a6dac2793544afeba28604c86ec041f
Dortmund-Reus <wjxxwh@126.com>
2020-09-02 16:03:34 +0800

    first commit

---
+++ kk.h
@@ -0,0 +1,61 @@
+ ///
+ /// @file    kk.h
+ /// @author  lemon(dortmund_reus@163.com)
+ /// @date    2020-08-29 19:27:01
+ ///
+ 
+struct STime
+{
+	unsigned char ucYear;
+	unsigned char ucMonth;
+	unsigned char ucDay;
+	unsigned char ucHour;
+	unsigned char ucMinute;
+	unsigned char ucSecond;
+	unsigned short usMiliSecond;
+};
+struct SAcc
+{
+	short a[3];
+	short T;
+};
+struct SGyro
+{
+	short w[3];
+	short T;
+};
+struct SAngle
+{
+	short Angle[3];
+	short T;
+};
+struct SMag
+{
+	short h[3];
+	short T;
+};
+
+struct SDStatus
+{
+	short sDStatus[4];
+};
+
+struct SPress
+{
+	long lPressure;
+	long lAltitude;
+};
+
+struct SLonLat
+{
+	long lLon;
+	long lLat;
+};
+
+struct SGPSV
+{
+	short sGPSHeight;
+	short sGPSYaw;
+	long lGPSVelocity;
+};
+
